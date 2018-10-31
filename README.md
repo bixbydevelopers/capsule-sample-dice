640a43856d555fc75c67f4d244ea56d9fcbdb1e2
John Colarusso <john.colarusso@samsung.com>
2018-10-31 10:26:32 -0700

    Dice Capsule

---
+++ README.md
@@ -0,0 +1,4 @@
+# Dice Capsule
+
+This capsule is for documentation purposes and is referenced in the [Quick Start](https://bixbydevelopers.com/dev/docs/get-started/quick-start).
+
---
+++ code/RollDice.js
@@ -0,0 +1,26 @@
+// RollDice
+// Rolls a dice given a number of sides and a number of dice
+
+// Main entry point
+function rollDice(numDice, numSides) {
+
+  var sum = 0;
+  var result = [];
+
+  for (var i = 0; i < numDice; i++) {
+    var roll = Math.ceil(Math.random() * numSides);
+    result.push(roll);
+    sum += roll;
+  }
+
+  // RollResult
+  return {
+    sum: sum, // required Sum
+    roll: result // required list Roll
+  }
+}
+
+// Exports
+module.exports = {
+  function: rollDice
+}
